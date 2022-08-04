6de261ca1c140ddeefd7e1e81374287079260641
biboy1999 <biboy1999@gmail.com>
2022-08-05 03:52:21 +0800

    move import line

--- hn_transform/app.py
+++ hn_transform/app.py
@@ -1,10 +1,10 @@
-import hn_transform.api
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+import hn_transform.api
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 5000, debug=True)
